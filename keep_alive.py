8e0c40ab792ba0a2baaf2c41c38ac33a771a793e
Julik <juliananisia2008@gmail.com>
2026-05-11 18:48:18 +0200

    fix HEAD request

--- keep_alive.py
+++ keep_alive.py
@@ -6,6 +6,11 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"ok")
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
     def log_message(self, *args):
         pass
 
